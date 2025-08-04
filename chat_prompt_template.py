4cd1c49c00a4e1bffb5e617f6f8dbec212c1620f
Javeria Iqbal <javerialareb249@gmail.com>
2025-08-04 10:57:10 +0500

    Add files via upload

---
+++ chat_prompt_template.py
@@ -0,0 +1,9 @@
+from langachain_core.prompts import ChatPromptTemplate
+
+chat_template=ChatPromptTemplate([
+      ('system', 'You are a helpful {domain} expert'),
+    ('human', 'Explain in simple terms, what is {topic}') 
+])
+prompt=chat_template.invoke({'domain':'cricket','topic':'Dusra'})
+
+print(prompt)
